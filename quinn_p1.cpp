e107c21ec1b94d2d7967ecb13a8acd0dfdd95386
Michael Quinn <mquinn5@binghamton.edu>
2018-09-22 22:54:30 -0400

    renamed/readded main files

---
+++ quinn_p1.cpp
@@ -0,0 +1,323 @@
+#include "Turing.h"
+#include "Filereader.h"
+#define DEBUG false
+
+using namespace std;
+
+vector<string> parser(string str){
+	vector<string> toReturn = vector<string>();
+	string s = "";
+	for(unsigned int i=0; i<str.size(); i++){
+		char c = str.at(i);
+		if(c=='\t'){
+			if(s.size()>0){
+				toReturn.push_back(s);
+				s="";
+			}
+		} else {
+			s.append(1, c);
+		}
+	}
+	if(s.size()>0)
+		toReturn.push_back(s);
+	return toReturn;
+}
+
+int main(int argc, char *argv[]){
+	if(argc!=4){ //if bad command
+		if(argc<4)
+			cout << "ERROR: Not enough arguments" << endl;
+		else
+			cout << "ERROR: Too many arguments" << endl;
+		return 1;
+	}
+	ifstream file;
+	file.open(argv[1]);
+	if(!file.is_open()){
+		cout << "ERROR: Bad file" << endl;
+		return 1;
+	}
+	FileReader reader(&file);
+	if(!reader.readFile()){
+		cout << "ERROR: Bad File Reader" << endl;
+		return 1;
+	}
+	
+	unsigned long max = stoul(argv[3], nullptr, 10);
+	
+	Turing turing = Turing(argv[2], max);
+	
+	if(DEBUG){
+		reader.start();
+		cout << "Printing Text:" << endl;
+		while(!reader.atEnd()){
+			cout << reader.current() << endl;
+			reader.next();
+		}
+		cout << "Printed Text!\n" << endl;
+	}
+	
+	reader.start();
+	while(!reader.atEnd()){
+		string str = reader.current();
+		if(DEBUG)
+			cout << "Start\t" << str << endl;
+		if(str.find("state", 0)==0){
+			if(DEBUG)
+				cout << "\tState: Adding... ";
+			turing.addState(parser(str));
+		} else if(str.find("transition", 0)==0){
+			if(DEBUG)
+				cout << "\tTransition: Adding... " << endl;
+			turing.addTrans(parser(str));
+		}
+		if(DEBUG)
+			cout << "\t\tAdded!\t" << str << endl;
+		reader.next();
+	}
+	
+	turing.start();
+	
+	return 0;
+}
+
+Turing::Turing(string str, unsigned long max){
+	this->states=vector<State>();
+	this->cur=-1;
+	this->start=-1;
+	this->str=str;
+	this->index=0;
+	this->counter=0;
+	this->max=max;
+}
+
+//Entry into program
+bool Turing::start(){
+	this->cur=this->start;
+	cout << this->cur;
+	if(!this->go()){
+		cout << " quit" << endl;
+		return false;
+	}
+	return true;
+}
+
+bool Turing::go(){
+	if(this->counter++>this->max)
+		return false;
+	State * s = getState(this->cur);
+	if(s->isAccept()){
+		cout << " accept" << endl;
+		return true;
+	} else if(s->isReject()){
+		cout << " reject" << endl;
+		return true;
+	}
+	char c = this->str.at(this->index);
+	if(s->hasTrans(c)){
+		Transition * t = s->getTrans(c);
+		this->cur=t->getR();
+		this->str.replace(this->index, 1, 1, t->getB());
+		if(t->getX()=='L'){
+			if(this->index==0)
+				return false;
+			else
+				this->index--;
+		} else if(t->getX()=='R'){
+			if(this->index==this->str.size()-1)
+				return false;
+			else
+				this->index++;
+		} else
+			return false;
+		cout << "->" << this->cur;
+		return this->go();
+	}
+	return false;
+}
+
+bool Turing::addState(State s){
+	if(this->hasState(s.getNum())){
+		State s0 = *(this->getState(s.getNum()));
+		return s0.equals(s);
+	}
+	if(s.isStart()){
+		if(this->hasStart())
+			return false;
+		this->start=s.getNum();
+		this->cur=this->start;
+	}
+	this->states.push_back(s);
+	return true;
+}
+
+bool Turing::addState(vector<string> str){
+	State s = State(str);
+	return this->addState(s);
+}
+
+bool Turing::hasState(int num) const{
+	for(unsigned int i=0; i<this->states.size(); i++){
+		if(this->states[i].getNum()==num)
+			return true;
+	}
+	return false;
+}
+
+State * Turing::getState(int num){
+	for(unsigned int i=0; i<this->states.size(); i++){
+		if(this->states[i].getNum()==num)
+			return &(this->states[i]);
+	}
+	return NULL;
+}
+
+bool Turing::hasTrans(int num, char a) const{
+	for(unsigned int i=0; i<this->states.size(); i++){
+		if(this->states[i].getNum()==num)
+			return this->states[i].hasTrans(a);
+	}
+	return false;
+}
+
+Transition * Turing::getTrans(int num, char a){
+	for(unsigned int i=0; i<this->states.size(); i++){
+		if(this->states[i].getNum()==num)
+			return this->states[i].getTrans(a);
+	}
+	return NULL;
+}
+
+bool Turing::addTrans(Transition t){
+	if(this->hasTrans(t.getQ(), t.getA())){
+		Transition t0 = *(this->getTrans(t.getQ(), t.getA()));
+		return t0.equals(t);
+	}
+	if(this->hasState(t.getQ())){
+		this->getState(t.getQ())->addTrans(t);
+		return true;
+	}
+	this->addState(State(t.getQ()));
+	return true;
+}
+
+bool Turing::addTrans(vector<string> str){
+	Transition t = Transition(str);
+	return this->addTrans(t);
+}
+
+
+
+State::State(vector<string> str){
+	string s = "state";
+	if(DEBUG)
+		cout << "(str.size()==" << str.size() << "),[num=";
+	this->num = stoi(str[1], nullptr, 10);
+	if(DEBUG)
+		cout << this->num << "]";
+	
+	this->start=this->accept=this->reject=false;
+	for(unsigned int i=2; i<str.size(); i++){
+		if(DEBUG)
+			cout << ",(str[" << i << "]==" << str[i] << ")";
+		s = "start";
+		if(str[i].compare(s)==0){
+			this->start=true;
+			if(DEBUG)
+				cout << ",[" << s << "]";
+		}
+		s = "accept";
+		if(str[i].compare(s)==0){
+			this->accept=true;
+			if(DEBUG)
+				cout << ",[" << s << "]";
+		}
+		s = "reject";
+		if(str[i].compare(s)==0){
+			this->reject=true;
+			if(DEBUG)
+				cout << ",[" << s << "]";
+		}
+	}
+	if(DEBUG)
+		cout << endl;
+}
+
+State::State(int num, bool start, bool accept, bool reject){
+	this->num=num;
+	this->start=start;
+	if((!accept ||  !reject) || !(accept && reject)){
+		this->accept = accept;
+		this->reject = reject;
+	}
+}
+
+State::State(int num){
+	this->num=num;
+	this->start=this->accept=this->reject=false;
+}
+
+bool State::equals(State o) const {
+	return o.getNum()==this->getNum() && o.isStart()==this->isStart() && o.isAccept()==this->isAccept() && o.isReject()==this->isReject();
+}
+
+bool State::hasTrans(char a) const {
+	for(unsigned int i=0; i<this->trans.size(); i++){
+		if(this->trans[i].getA()==a)
+			return true;
+	}
+	return false;
+}
+
+Transition * State::getTrans(char a){
+	for(unsigned int i=0; i<this->trans.size(); i++){
+		if(this->trans[i].getA()==a)
+			return &(this->trans[i]);
+	}
+	return NULL;
+}
+
+bool State::addTrans(Transition t){
+	if(this->hasTrans(t.getA())){
+		Transition t0 = *(this->getTrans(t.getA()));
+		return t0.equals(t);
+	}
+	this->trans.push_back(t);
+	return true;
+}
+
+bool State::addTrans(vector<string> str){
+	Transition t = Transition(str);
+	return this->addTrans(t);
+}
+
+
+
+Transition::Transition(vector<string> str){
+	string t = "transition\t";
+	if(DEBUG)
+		cout << "[q=";
+	this->q = stoi(str[1], nullptr, 10);
+	if(DEBUG)
+		cout << this->q << "],[a=";
+	this->a = str[2].at(0);
+	if(this->a=='_')
+		this->a=' ';
+	if(DEBUG)
+		cout << this->a << "],[r=";
+	this->r= stoi(str[3], nullptr, 10);
+	if(DEBUG)
+		cout << this->r << "],[b=";
+	this->b = str[4].at(0);
+	if(this->b=='_')
+		this->b=' ';
+	if(DEBUG)
+		cout << this->b << "],[x=";
+	this->x = str[5].at(0);
+	if(DEBUG)
+		cout << this->x << "]" << endl;
+}
+
+bool Transition::equals(Transition o) const {
+	return o.getQ()==this->getQ() && o.getA()==this->getA() && o.getR()==this->getR() && o.getB()==this->getB() && o.getX()==this->getX();
+}
---
+++ quinn_p1.h
@@ -0,0 +1,76 @@
+#ifndef MQ_TURING_H
+#define MQ_TURING_H
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <vector>
+#include <string>
+
+class Transition{
+	private:
+		int q;
+		char a;
+		int r;
+		char b;
+		char x;
+		friend class State;
+	public:
+		Transition(std::vector<std::string> str);
+		bool equals(Transition o) const;
+		int getQ()const{return this->q;};
+		char getA()const{return this->a;};
+		int getR()const{return this->r;};
+		char getB()const{return this->b;};
+		char getX()const{return this->x;};
+};
+
+class State{
+	private:
+		bool start;
+		bool accept;
+		bool reject;
+		int num;
+		std::vector<Transition> trans;
+		friend class Turing;
+		Transition * getTrans(char a);
+	public:
+		State(std::vector<std::string>);
+		State(int num, bool start, bool accept, bool reject);
+		State(int num);
+		bool equals(State o) const;
+		bool addTrans(Transition t);
+		bool addTrans(std::vector<std::string> str);
+		bool hasTrans(char a) const;
+		int getNum()const{return this->num;};
+		bool isStart()const{return this->start;};
+		bool isAccept()const{return this->accept;};
+		bool isReject()const{return this->reject;};
+};
+
+class Turing{
+	private:
+		std::vector<State> states;
+		int cur;
+		int start;
+		int index;
+		unsigned long counter;
+		unsigned long max;
+		State * getState(int num);
+		Transition * getTrans(int num, char a);
+		bool go();
+	public:
+		Turing(std::string str, unsigned long max);
+		std::string str;
+		bool hasStart(){return this->start!=-1;};
+		int getCur(){return this->cur;};
+		bool addState(State s);
+		bool addState(std::vector<std::string> str);
+		bool hasState(int num) const;
+		bool hasTrans(int num, char a) const;
+		bool addTrans(Transition t);
+		bool addTrans(std::vector<std::string> str);
+		bool start();
+};
+
+#endif
