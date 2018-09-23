965189d247c5be5f7d03d49ea3e822aba41c1d45
Michael Quinn <mquinn5@binghamton.edu>
2018-09-22 23:36:55 -0400

    undid previous change

--- quinn_p1.cpp
+++ quinn_p1.cpp
@@ -154,7 +154,7 @@ bool Turing::go(char x){
 			} else
 				this->index--;
 		} else if(x=='R'){
-			if(((unsigned int) this->index)==this->str.size()){
+			if(((unsigned int) this->index)==this->str.size()-1){
 				s->print();
 				return false;
 			} else
