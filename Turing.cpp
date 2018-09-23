ff8ed9fc90bf068121779b16f9586fff333b1bf8
Michael Quinn <mquinn5@binghamton.edu>
2018-09-22 21:36:00 -0400

    debugging

--- Turing.cpp
+++ Turing.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]){
 	if(DEBUG){
 		reader.start();
 		while(!reader.atEnd()){
-			cout << str << endl;
+			cout << reader.current() << endl;
 			reader.next();
 		}
 	}
